240c8e9ac0473b7c1c4cf38986c2f61e53bfa696
Julien Ricordeau <julien.ricordeau@voila.fr>
2015-06-05 18:21:53 +0200

    fin tuto blog

--- js/blog.js
+++ js/blog.js
@@ -3,11 +3,29 @@ $(function() {
     Parse.$ = jQuery;
  
   	Parse.initialize("SfzcjIxCxeNmXxhxZeMFWwToy9O5gDpm6DeD4dEa", "sjxy2Ws36g8BhjTv6FUJ2Bvcmgjq5I23TYhieOQN");
-  	
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
-    });
- 
+
+   var Blog = Parse.Object.extend("Blog"),
+		Blogs = Parse.Collection.extend({
+			model: Blog
+		}),
+		BlogsView = Parse.View.extend({
+			template: Handlebars.compile($('#blogs-tpl').html()),
+			render: function() { 
+				var collection = { blog: this.collection.toJSON() };
+				this.$el.html(this.template(collection));
+			}
+		});
+		blogs = new Blogs();
+
+		blogs.fetch({
+			success: function(blogs) {
+				var blogsView = new BlogsView({ collection: blogs });
+				blogsView.render();
+				$('.main-container').html(blogsView.el);
+			},
+			error: function(blogs, error) {
+				console.log(error);
+			}
+		});
+
 });
